e0e6e355c53215a75145a26001aa436b847204db
Bora Jin <jinbora0720@gmail.com>
2020-12-05 23:35:17 -0500

    build website

---
+++ js/wowchemy-init.js
@@ -0,0 +1,49 @@
+(() => {
+  // ns-hugo:/var/folders/gc/25qwth7n2m96x1y59ltt6yw80000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201205224343-d70da8919962/assets/js/wowchemy-theming.js
+  function getThemeMode() {
+    return parseInt(localStorage.getItem("dark_mode") || 2);
+  }
+  function canChangeTheme() {
+    return Boolean(window.wc.darkLightEnabled);
+  }
+  function initThemeVariation() {
+    if (!canChangeTheme()) {
+      return;
+    }
+    let currentThemeMode = getThemeMode();
+    let isDarkTheme;
+    switch (currentThemeMode) {
+      case 0:
+        isDarkTheme = false;
+        break;
+      case 1:
+        isDarkTheme = true;
+        break;
+      default:
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          isDarkTheme = true;
+        } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+          isDarkTheme = false;
+        } else {
+          isDarkTheme = window.wc.isSiteThemeDark;
+        }
+        break;
+    }
+    if (isDarkTheme) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }
+
+  // ns-params:@params
+  var wcDarkLightEnabled = true;
+  var wcIsSiteThemeDark = false;
+
+  // js/wowchemy-init.js
+  window.wc = {
+    darkLightEnabled: wcDarkLightEnabled,
+    isSiteThemeDark: wcIsSiteThemeDark
+  };
+  initThemeVariation();
+})();
